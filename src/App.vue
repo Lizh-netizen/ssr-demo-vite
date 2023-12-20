943ed8efb762416ca8356854e2fb9d94fa049bf5
regina <17839228268@163.com>
2023-12-20 16:33:56 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
   </ElConfigProvider>
 </template>
 <script setup>
+  import { nextTick } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
 import { ElConfigProvider } from 'element-plus'
 
 // 国际中文
 
 import EleZhCn from 'element-plus/dist/locale/zh-cn.mjs'
+nextTick(() => {
+  window.addEventListener('scroll', () => {
+    console.log('scrol')
+  })
+})
+
 </script>
