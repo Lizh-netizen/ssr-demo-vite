24be0617162b8c108fe43e8eaca17021d574f8af
regina <17839228268@163.com>
2024-01-09 15:32:50 +0800

    问题排布

---
+++ src/components/layout/draggable.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <draggable
+      class="list-group"
+      v-model="data"
+      @start="drag = true"
+      @end="drag = false"
+      group="people"
+      item-key="id"
+    >
+      <template #item="{ element }">
+        <div class="list-group-item">
+          <img src="../../assets/layout/drag.svg" />
+
+          <div class="list-group-item-name">{{ element.name }}</div>
+        </div>
+      </template>
+    </draggable>
+  </div>
+</template>
+
+<script setup>
+import draggable from 'vuedraggable'
+import { watch, defineProps, ref } from 'vue'
+const props = defineProps({
+  list: {
+    type: Array,
+    default: () => []
+  }
+})
+const data = ref(props.list)
+
+watch(
+  props,
+  (val) => {
+    data.value = val.list
+  },
+  { deep: true }
+)
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+}
+.list-group {
+  margin-top: 18px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.list-group-item {
+  width: 294px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 8px;
+  /* background: #E3EBFA; */
+  background: #ecedee;
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+  margin-right: 8px;
+}
+</style>
