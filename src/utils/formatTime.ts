c02c093ec1b8fd8c42d93f04be8c78b41453ee20
regina <17839228268@163.com>
2024-01-31 16:59:34 +0800

    修改format时间

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,21 +1,21 @@
+export default function formatTime() {
+  const today = new Date()
+  let year = today.getFullYear()
+  let month = today.getMonth() + 1
+  // @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
+  month = month < 10 ? `0${month}` : month // 确保月份是两位数
+  let day = today.getDate()
+  // @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
+  day = day < 10 ? `0${day}` : day // 确保日期是两位数
+  let lastMonth = today.getMonth()
 
-
-export default function formatTime () {
-const today = new Date();
-const year = today.getFullYear();
-let month = today.getMonth() + 1;
-// @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
-month = month < 10 ? `0${month}` : month; // 确保月份是两位数
-let day = today.getDate();
-// @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
-day = day < 10 ? `0${day}` : day; // 确保日期是两位数
-let lastMonth = today.getMonth();
-
-if (lastMonth === 0) {
-  lastMonth = 12;
-}
-  let firstDate = `${year}-${lastMonth}-01`;
-const formattedToday = `${year}-${month}-${day}`;
+  if (lastMonth === 0) {
+    lastMonth = 12
+  }
+  if (lastMonth === 12) {
+    year = year - 1
+  }
+  let firstDate = `${year}-${lastMonth}-01`
+  const formattedToday = `${year}-${month}-${day}`
   return { firstDate, formattedToday }
 }
-
