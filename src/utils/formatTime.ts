efcf57e0ef21987972886ccc4573bcef2e402715
regina <17839228268@163.com>
2024-02-01 10:57:27 +0800

    format时间修改

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -5,11 +5,14 @@ export default function formatTime() {
   // @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
   month = month < 10 ? `0${month}` : month // 确保月份是两位数
   let day = today.getDate()
+  console.log('🚀 ~ formatTime ~  month:', typeof month)
   // @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
   day = day < 10 ? `0${day}` : day // 确保日期是两位数
-  let lastMonth = today.getMonth()
+  let lastMonth = today.getMonth() + 0
+  // @ts-expect-error TS(2322): Type 'string | number' is not assignable to type '... Remove this comment to see the full error message
+  lastMonth = lastMonth < 10 ? `0${lastMonth}` : lastMonth
 
-  if (lastMonth === 0) {
+  if (lastMonth == 0) {
     lastMonth = 12
   }
   if (lastMonth === 12) {
